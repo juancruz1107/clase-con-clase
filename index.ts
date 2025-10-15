b7b9b545411488042eabc31724c7a008a385bb69
Juan Cruz <juancrush1107@gmail.com>
2025-10-15 17:16:45 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+class Album{
+    title:string
+    songs:string[]
+    
+    constructor(title:string, songs:string[]){
+        this.title = title;
+        this.songs = songs;
+    }
+}
+
+class Banda{
+    members:string[]
+    albums:Album[]
+    constructor(members:string[], albums:Album[]){
+        this.members = members;
+        this.albums = albums; 
+    }
+}
+function main(){
+    const unAlbum = new Album("41", ["Te Vi y Temblé", "Causa", "Rompe los Techos"])
+    
+    const unaBanda = new Banda(["Seba", "Morfi","Rodri","Brian","Samu"], [unAlbum])
+    
+    //console.log(unaBanda.members)
+    
+    console.log(unaBanda.albums )
+}
+
+main()
